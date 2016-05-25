a7112a6b4cb9e71cf960cf73a491bc9b372d9f4b
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-05-25 10:23:51 -0400

    Added keypress events for play, next and previous

--- browser/js/module.js
+++ browser/js/module.js
@@ -11,3 +11,26 @@ juke.run(function ($rootScope) {
 juke.config(function ($urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 })
+
+juke.directive('keypress', function(){
+  return{
+    restrict: 'A',
+    link:function(s,e,a){
+      e.bind("keydown", function (event) {
+        // console.log("You pressed a key:", event.which);
+        // console.log("currentSong is", s.getCurrentSong());
+            if(event.which === 39) {
+              console.log("pressing the right button");
+              s.next();
+            }
+            else if (event.which === 37) {
+              s.previous();
+            }
+            else if( event.which === 32){
+              s.toggle();
+            }
+            event.preventDefault();
+        });
+    }
+  }
+})
